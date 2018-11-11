35ad1f70308cc432f28e0a17bd0232d76ffc438b
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2018-11-10 19:08:23 -0500

    render a board for snek

--- src/Games/Snake/Model.elm
+++ src/Games/Snake/Model.elm
@@ -1,10 +1,18 @@
 module Games.Snake.Model exposing (Model, init)
 
 
+type alias Point =
+    ( Float, Float )
+
+
 type alias Model =
-    {}
+    { body : List Point
+    , foodPosition : Point
+    }
 
 
 init : Model
 init =
-    {}
+    { body = [ ( 0, 0 ) ]
+    , foodPosition = ( 10, 1 )
+    }
--- src/Games/Snake/View.elm
+++ src/Games/Snake/View.elm
@@ -1,32 +1,46 @@
 module Games.Snake.View exposing (view)
 
+import Collage exposing (..)
+import Collage.Layout exposing (..)
+import Collage.Render exposing (svg)
+import Color
 import Games.Snake.Model exposing (Model)
 import Games.Snake.Update exposing (Msg(..))
 import Html exposing (Html)
-import Svg exposing (circle, rect, svg)
-import Svg.Attributes exposing (cx, cy, height, r, rx, ry, viewBox, width, x, y)
 
 
-view : Model -> Html Msg
+boardHeight : Float
+boardHeight =
+    700
+
+
+boardWidth : Float
+boardWidth =
+    500
+
+
+snakeSegmentSize : Float
+snakeSegmentSize =
+    1
+
+
+view : Model -> Html msg
 view model =
-    svg
-        [ width "120"
-        , height "120"
-        , viewBox "0 0 120 120"
-        ]
-        [ rect
-            [ x "10"
-            , y "10"
-            , width "100"
-            , height "100"
-            , rx "15"
-            , ry "15"
-            ]
-            []
-        , circle
-            [ cx "50"
-            , cy "50"
-            , r "50"
-            ]
-            []
-        ]
+    let
+        boardRect : Collage msg
+        boardRect =
+            rectangle boardWidth boardHeight
+                |> outlined (solid 2 (uniform Color.black))
+
+        snek : Collage msg
+        snek =
+            square snakeSegmentSize
+                |> filled (uniform Color.green)
+
+        food : Collage msg
+        food =
+            square snakeSegmentSize
+                |> filled (uniform Color.darkCharcoal)
+    in
+    boardRect
+        |> svg
