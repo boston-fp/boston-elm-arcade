dd02905bbb37adac666a02d668cc88f9631cb217
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2018-11-27 20:19:21 -0500

    Add saving through a port

--- src/Ports.elm
+++ src/Ports.elm
@@ -1,4 +1,15 @@
 port module Ports exposing (logToConsole)
 
+import Json.Encode as E
+
 
 port logToConsole : String -> Cmd msg
+
+
+{-| save arbitary json to js
+-}
+port save : SaveData -> Cmd msg
+
+
+type alias SaveData =
+    { gameName : String, data : E.Value }
